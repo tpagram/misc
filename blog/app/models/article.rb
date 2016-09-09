4307a44e68efdaf56870856544ee09b7ba1b1bf5
Thomas Pagram <tpagram@gmail.com>
2016-09-09 19:08:46 +0800

    blog: added tag feature

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,3 +1,16 @@
 class Article < ApplicationRecord
 	has_many :comments
+	has_many :taggings
+	has_many :tags, through: :taggings
+
+	def tag_list
+  		tags.join(", ")
+	end
+
+	def tag_list=(tags_string)
+		tags = tags_string.split(',').collect{|s| s.strip.downcase}.uniq
+		new_or_existing_tags = tags.collect{|s| Tag.find_or_create_by(name: s)}
+		self.tags = new_or_existing_tags
+	end
+
 end
